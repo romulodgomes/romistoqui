bc5a4198f931f2c9d3500ae6e7b8a8a3eba94a74
Rômulo Gomes <romulo.redg@gmail.com>
2020-09-18 02:02:51 -0300

    Criação de pagina de cadastro de item

--- src/pages/Itens/index.js
+++ src/pages/Itens/index.js
@@ -1,12 +1,82 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, Picker, TouchableOpacity } from 'react-native';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+
+
 import Container from '../../components/Container';
 import Header from '../../components/Header';
 
+
+import styles from './styles';
+import api from '../../services/api';
+
 export default function Itens() {
+    const [nome, setNome] = useState('');
+    // const [qtd, setQtd] = useState('');
+    const [categoriaSelecionada, setCategoriaSelecionada] = useState('');
+    const [categorias, setCategorias] = useState([]);
+
+    async function loadCategorias(){
+        const response = await api.get('categorias');
+        setCategorias(response.data);
+    }
+
+    async function handleCadastrar() {
+        const data = {
+            nome,
+            categoria_id: categoriaSelecionada
+        }
+
+        try {
+            const response = await api.post('itens', data);
+
+            if (response.status==200)
+            alert('Item cadastrado com sucesso');
+
+        } catch (err) {
+            alert('Erro no cadastro, tente novamente')
+        }
+    }
+
+    useEffect(() => {
+        loadCategorias();
+    }, []);
+
     return (
         <Container>
             <Header />
+            <Text style={styles.title}>Adicionar Item</Text>
+            <ScrollView style={styles.formulario}>
+                <Text style={styles.labelCampo}>Nome do item:</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Escreva aqui o nome do item"
+                    onChangeText={text => setNome(text)}
+                    defaultValue={nome}
+                />
+                {/* <Text style={styles.labelCampo}>Quantidade:</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Escreva aqui a quantidade que você já possui"
+                    onChangeText={text => setQtd(text)}
+                    defaultValue={qtd}
+                /> */}
+                <Text style={styles.labelCampo}>Selecione a categoria do item:</Text>
+                <Picker
+                    selectedValue={categoriaSelecionada}
+                    style={styles.input}
+                    onValueChange={(itemValue, itemIndex) => setCategoriaSelecionada(itemValue)}
+                >
+                    <Picker.Item label='Selecione' value=''/>
+                    {categorias.map((categoria) => {
+                        return(<Picker.Item key={categoria.id} label={categoria.nome} value={categoria.id} />)
+                    })}
+                </Picker>
+                <TouchableOpacity style={styles.botao} onPress={handleCadastrar}>
+                    <Text style={styles.botaoText}>Cadastrar</Text>
+                </TouchableOpacity>
+                
+            </ScrollView>
         </Container>
     )
 }
---
+++ src/pages/Itens/styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: "center",
+        marginVertical: 20
+    },
+
+    formulario: {
+        paddingHorizontal: 20
+    },
+
+    labelCampo: {
+        fontSize: 18
+    },
+
+    input: {
+        fontSize: 18,
+        borderBottomWidth: 1,
+        marginBottom: 20
+    },
+
+    botao: {
+        backgroundColor: '#00b894',
+        height: 50,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems:'center'
+    },
+
+    botaoText: {
+        color: '#fff',
+        fontSize: 20
+    }
+});
